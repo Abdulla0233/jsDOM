0339aecccd749d57f5e2871a072f533de5ad2021
Abdulla0233 Valiyev <vliyevabdulla+demo@gmail.com>
2023-08-20 16:05:38 +0000

    for

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 "use strict";
 $(document).ready(function () {
 
+$('.btn').click(function (e) { 
+    e.preventDefault();
+    let k = k => {
+        for( let i=1; i<=k; i++) {
+            $('.end').append(
+                `<div class="res">
+                    <h3>${k}</h3>    
+                </div>`
+            );
+        }
+    }
+    k($('input').val());
+    });
 });
