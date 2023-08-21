04e11ba641a91c43d2062b26b599936096e364fc
Abdulla0233 Valiyev <vliyevabdulla+demo@gmail.com>
2023-08-21 10:12:36 +0000

    append

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 "use strict";
 $(document).ready(function () {
 
-$('.btn').click(function (e) { 
-    e.preventDefault();
-    let k = k => {
-        for( let i=1; i<=k; i++) {
-            $('.end').append(
-                `<div class="res">
-                    <h3>${k}</h3>    
-                </div>`
-            );
-        }
-    }
-    k($('input').val());
-    });
+    $('.btn').click(function (e) {
+        e.preventDefault();
+            let a = a => {
+                for (let i = 0; i < a; i++) { $('.end').append(`<h4>${a}</h4>`) };
+            }
+            let k = k => Number(k) ? a(k) : $('.end').append(`<h4>${k}</h4>`);
+            k($('input').val());
+        
+        $('input').val()
+    })
+
 });
