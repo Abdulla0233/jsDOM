df7ded27c41401244438bb94c10d42ba46ad6102
Abdulla0233 Valiyev <vliyevabdulla+demo@gmail.com>
2023-08-22 18:39:36 +0000

    trim

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ $(document).ready(function () {
 
     $('.btn').click(function (e) {
         e.preventDefault();
+        if (!$.trim($('input').val()) == '') {
             let a = a => {
                 for (let i = 0; i < a; i++) { $('.end').append(`<h4>${a}</h4>`) };
             }
             let k = k => Number(k) ? a(k) : $('.end').append(`<h4>${k}</h4>`);
             k($('input').val());
-        
+    }
         $('input').val()
     })
 
