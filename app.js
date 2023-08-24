9ec7c11eb31d57987c1432aaadd3b8dc0753cb18
Abdulla0233 Valiyev <vliyevabdulla+demo@gmail.com>
2023-08-24 20:13:48 +0000

    h4 color

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ $(document).ready(function () {
         e.preventDefault();
         if (!$.trim($('input').val()) == '') {
             let a = a => {
-                for (let i = 0; i < a; i++) { $('.end').append(`<h4>${a}</h4>`) };
+                for (let i = 0; i < a; i++) { $('.minibox').append(`<h4>${a}</h4>`) };
             }
-            let k = k => Number(k) ? a(k) : $('.end').append(`<h4>${k}</h4>`);
+            let k = k => Number(k) ? a(k) : $('.minibox').append(`<h4>${k}</h4>`);
             k($('input').val());
     }
-        $('input').val()
+        $('input').val('')
     })
 
 });
